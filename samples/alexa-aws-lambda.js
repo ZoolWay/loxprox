93afa499f09943f56250da7ff71c29563ae0be45
ZoolWay <zoolway@gmail.com>
2017-02-15 22:42:23 +0100

    fix: fixed lambda function to really complete AWS IoT request

--- samples/alexa-aws-lambda.js
+++ samples/alexa-aws-lambda.js
@@ -10,42 +10,45 @@ const Alexa = require('alexa-sdk');
 const AWS = require('aws-sdk');
 
 var config = {};
-config.IOT_BROKER_ENDPOINT = "CUSTOM.iot.eu-central-1.amazonaws.com".toLowerCase();
-config.IOT_BROKER_REGION = "eu-central-1";
+config.IOT_BROKER_ENDPOINT = "<CUSTOM>.iot.<REGION>.amazonaws.com".toLowerCase(); //e.g. region = eu-west-1
+config.IOT_BROKER_REGION = "<REGION>";
 config.IOT_THING_NAME = "loxprox";
+config.IOT_TOPIC = "messages";
 
 AWS.config.region = config.IOT_BROKER_REGION;
 var iotData = new AWS.IotData({endpoint: config.IOT_BROKER_ENDPOINT});
 
 const APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
 
+function publishSignal(signal, cbOkay, cbError) {
+    console.log(`Publishing signal '${signal}' to topic '${config.IOT_TOPIC}'`);
+    let payload = {
+      signal: signal  
+    };
+    let params = {
+        topic: config.IOT_TOPIC,
+        qos: 0,
+        payload: JSON.stringify(payload)
+    };
+    let request = iotData.publish(params, function(err, data) {
+        if (err) {
+            console.error('Failed to publish to IoT:', err);
+            if (cbError) cbError();
+        } else {
+            if (cbOkay) cbOkay();
+        }
+    });
+}
+
 const handlers = {
     'LaunchRequest': function() {
       this.emit(':tell', 'Willkommen bei Loxone');
     },
-    'LightOn': function () {
-        var payload = {
-            signal: 'light-on'
-        };
-        var params = {
-          topic: 'messages',
-          qos: 0,
-          payload: JSON.stringify(payload)
-        };
-        iotData.publish(params);
-        this.emit(':tell', 'Licht eingeschaltet');
+    'LightOnIntent': function () {
+        publishSignal('light-on', () => this.emit(':tell', 'Licht eingeschaltet'), () => this.emit(':tell', 'Verbindung nicht möglich'));
     },
-    'LightOff': function () {
-        var payload = {
-            signal: 'light-off'
-        };
-        var params = {
-          topic: 'messages',
-          qos: 0,
-          payload: JSON.stringify(payload)
-        };
-        iotData.publish(params);
-        this.emit(':tell', 'Licht eingeschaltet');
+    'LightOffIntent': function () {
+        publishSignal('light-off', () => this.emit(':tell', 'Licht ausgeschaltet'), () => this.emit(':tell', 'Verbindung nicht möglich'));
     },    
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', 'Auf Wiedersehen!');
